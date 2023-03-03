970b9281a5c79d4c84bfd43dd7c9308cf428a594
Alvin Galit <aroblesgalit@gmail.com>
2023-03-02 22:45:05 -0800

    Learning about different types

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,29 @@
 import React from 'react';
 import './App.css';
 
+let name: string;
+let age: number | string;
+let isStudent: boolean;
+let hobbies: string[];
+let role: [number, string]; //tuple
+
+let printName: (name: string) => never; //void vs never
+
+// Object
+// type Person = {
+//   name: string;
+//   age?: number; // Optional
+// };
+
+// let person: Person = {
+//   name: 'alvin',
+//   // age: 36
+// }
+
+// let lotsOfPeople: Person[];
+
+let personName: unknown;
+
 function App() {
   return (
     <div className="App">
