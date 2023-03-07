f3c4048d1790858d7fd7cdc8cd66b208b37fe5c1
Alvin Galit <aroblesgalit@gmail.com>
2023-03-06 23:10:31 -0800

    Removed practive code and added span element to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,10 @@
 import React from 'react';
 import './App.css';
 
-let name: string;
-let age: number | string;
-let isStudent: boolean;
-let hobbies: string[];
-let role: [number, string]; //tuple
-
-let printName: (name: string) => never; //void vs never
-
-// Object
-// type Person = {
-//   name: string;
-//   age?: number; // Optional
-// };
-
-// let person: Person = {
-//   name: 'alvin',
-//   // age: 36
-// }
-
-// let lotsOfPeople: Person[];
-
-let personName: unknown;
-
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      Hello World
+    <div className='App'>
+      <span className='heading'>Taskify</span>
     </div>
   );
 }
