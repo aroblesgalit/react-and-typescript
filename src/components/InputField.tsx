6b32a81e7dad48d3e86bea6fea3df3cb9fa43137
Alvin Galit <aroblesgalit@gmail.com>
2023-03-07 21:58:56 -0800

    Added styles

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import './styles.css'
 
 export default function InputField() {
   return (
     <form className='input'>
         <input type='input' placeholder='Enter  a task' className='input__box' />
+        <button className='input_submit' type='submit'>Go</button>
     </form>
   )
 }
