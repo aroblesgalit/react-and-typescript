a682e91dda9eb33fe9f370a3d15027a56fc6393a
Alvin Galit <aroblesgalit@gmail.com>
2023-03-07 22:04:52 -0800

    Created state and passed down props

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import './styles.css'
 
-export default function InputField() {
+interface Props {
+    todo: string;
+    setTodo: React.Dispatch<React.SetStateAction<string>>
+}
+
+export default function InputField({ todo, setTodo }: Props) {
   return (
     <form className='input'>
-        <input type='input' placeholder='Enter  a task' className='input__box' />
+        <input 
+            type='input' 
+            placeholder='Enter  a task' 
+            className='input__box' 
+            value={todo}
+            onChange={(e) => setTodo(e.target.value)}
+        />
         <button className='input_submit' type='submit'>Go</button>
     </form>
   )
