8c28bffc6a49b5d01bf0ed554b918af1cc8a429d
Alvin Galit <aroblesgalit@gmail.com>
2023-03-08 23:44:03 -0800

    Created function for handling add

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,23 @@ const App: React.FC = () => {
   const [todo, setTodo] = useState<string>('')
   const [todos, setTodos] = useState<Todo[]>([])
 
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if(todo) {
+      setTodos([...todos, {
+        id: Date.now(),
+        todo: todo,
+        isDone: false
+      }])
+    }
+    
+  }
+
   return (
     <div className='App'>
       <span className='heading'>Taskify</span>
-      <InputField todo={todo} setTodo={setTodo} />
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
     </div>
   );
 }
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -4,11 +4,12 @@ import './styles.css'
 interface Props {
     todo: string;
     setTodo: React.Dispatch<React.SetStateAction<string>>
+    handleAdd: (e: React.FormEvent) => void
 }
 
-export default function InputField({ todo, setTodo }: Props) {
+export default function InputField({ todo, setTodo, handleAdd }: Props) {
   return (
-    <form className='input'>
+    <form className='input' onSubmit={handleAdd}>
         <input 
             type='input' 
             placeholder='Enter  a task' 
